88ef7a3b7c954a322ecc788e185063bc83cbc80e
MKhai205 <manhkhai123459876@gmail.com>
2024-05-03 00:11:26 +0700

    add 4_3

--- 4_3_longestSequence.cpp
+++ 4_3_longestSequence.cpp
@@ -2,6 +2,31 @@
 
 using namespace std;
 
+int find(vector<int> a, int i, int j){
+    int k = a[j] - a[i], count = 2;
+    for(int x = j+1; x<a.size(); x++){
+        if(a[x] - a[i] == k*count) count++;
+    }
+    return count;
+}
+
+int longestSequence(vector<int> a){
+    int max = 0;
+    for(int i=0; i<a.size(); i++){
+        for(int j=i+1; j<a.size(); j++){
+            int tmp = find(a, i, j);
+            if(max < tmp){
+                max = tmp;
+            }
+        }
+    }
+    return max;
+}
+
 int main(){
     int n; cin >> n;
+    vector<int> arr(n);
+    for(int &x : arr) cin >> x;
+    cout << longestSequence(arr) << endl;
+    return 0;
 }
