0c1374d1d77f69beaeba3d98f9a6b5dd5abcb9a7
MKhai205 <manhkhai123459876@gmail.com>
2024-05-06 14:58:27 +0700

    fixed maxDifference

--- maxDifference.cpp
+++ maxDifference.cpp
@@ -36,8 +36,7 @@ int maxDifference2(vector<int> arr, int arr_size){
       
      // Minimum number visited so far 
      int min_element = arr[0];
-     for(int i = 1; i < arr_size; i++)
-     {     
+     for(int i = 1; i < arr_size; i++) {     
        if (arr[i] - min_element > max_diff)                             
        max_diff = arr[i] - min_element;
         
@@ -55,8 +54,7 @@ int maxDifference2(vector<int> arr, int arr_size){
    returns 0 if elements are equal */
 int maxDifference3(vector<int> arr, int arr_size){     
   int max_diff = arr[1] - arr[0];
-  for (int i = 0; i < arr_size; i++)
-  {
+  for (int i = 0; i < arr_size; i++) {
     for (int j = i+1; j < arr_size; j++)
     {     
       if (arr[j] - arr[i] > max_diff) 
@@ -66,6 +64,28 @@ int maxDifference3(vector<int> arr, int arr_size){
   return max_diff;
 } 
 
+/* The function assumes that there are 
+   at least two elements in array. The 
+   function returns a negative value if the
+   array is sorted in decreasing order and  
+   returns 0 if elements are equal */
+int maxDifference4(vector<int> arr, int arr_size){
+     // Maximum difference found so far
+     int max_diff = arr[1] - arr[0];
+      
+     // Minimum number visited so far 
+     int min_element = arr[0];
+     for(int i = 1; i < arr_size; i++) {     
+       if (arr[i] - min_element > max_diff)                             
+       max_diff = arr[i] - min_element;
+        
+       if (arr[i] < min_element)
+       min_element = arr[i];                     
+     }
+      
+     return max_diff;
+} 
+
 int main(){
     vector<int> arr = {2, 3, 10, 6, 4, 8, 1, 4, 3, 5, 23, 12, 
     3, 12, 1, 31, 1, 34, 13, 134, 1, 3, 13, 4, 34, 1, 41, 13, 
@@ -73,5 +93,6 @@ int main(){
     cout << maxDifference(arr, 0, arr.size() - 1) << endl;
     cout << maxDifference2(arr, arr.size()) << endl;
     cout << maxDifference3(arr, arr.size()) << endl;
+    cout << maxDifference4(arr, arr.size()) << endl;
     return 0;
 }
